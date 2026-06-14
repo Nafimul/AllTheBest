a24186e0b50b40c35a4c9e2b77114c09432632e7
nafim <islamnafimul@gmail.com>
2026-06-14 19:37:49 -0400

    feat: add login

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, flash, json, render_template, request
 from dotenv import load_dotenv
 
 from myapp.db.manager_exception import ManagerException
+from flask_login import current_user, login_required, login_user, logout_user, LoginManager
 from myapp.db.supabase_postgres_manager import SupabasePostgresManager
 
 def create_app(env="development"):
@@ -15,11 +16,22 @@ def create_app(env="development"):
     app.config.from_prefixed_env()
     app.app_context().push()
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+    login_manager.login_message = "You must be logged in to do that."
+
     db_manager = SupabasePostgresManager( os.environ.get("SUPABASE_URL"), os.environ.get("SUPABASE_KEY"))
 
     @app.route("/")
     def home():
+        # return [current_user.id, current_user.is_authenticated, current_user.email]
         return render_template("home.html")
+    
+    @login_manager.user_loader
+    def user_loader(id):
+        user = db_manager.get_user()
+        return user
     
     @app.route("/categories")
     def categories():
@@ -27,7 +39,7 @@ def create_app(env="development"):
         return render_template("categories.html", categories=categories)
     
     @app.route("/add-category")
-    def add_category():
+    def add_category_form():
         return render_template("add-category.html")
     
     @app.route("/category", methods=["POST"])
@@ -39,4 +51,27 @@ def create_app(env="development"):
             flash("Failed to add category")
         return render_template("add-category.html")
     
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
+            try:
+                user = db_manager.login(email, password)
+                login_user(user)
+                flash("Logged in successfully")
+                return render_template("home.html")
+            except ManagerException:
+                flash("Invalid email or password")
+                return render_template("login.html")
+
+        return render_template("login.html")
+    
+    @app.route("/profile", methods=["GET"])
+    def profile():
+        return render_template("profile.html", user = current_user)
+    
     return app
+
+    
+
--- myapp/db/supabase_postgres_manager.py
+++ myapp/db/supabase_postgres_manager.py
@@ -2,18 +2,19 @@ from postgrest import APIError
 from supabase import create_client, Client
 from myapp.db.manager_exception import ManagerException
 from myapp.models.category import Category
+from myapp.user import User
 
 class SupabasePostgresManager:
     def __init__(self, url, key):
-        self.url = url
-        self.key = key
+        self.supabase = create_client(
+            url, key
+        )
 
     def get_categories(self):
-        supabase: Client = create_client(
-            self.url,
-            self.key
-        )
-        response = supabase.table('categories').select("*").execute()
+        try:
+            response = self.supabase.table('categories').select("*").execute()
+        except APIError:
+            raise ManagerException("Failed to fetch categories")
         categories = []
         for item in response.data:
             category = Category(
@@ -25,16 +26,48 @@ class SupabasePostgresManager:
         return categories
     
     def add_category(self, category: Category, user_id: int):
-        supabase: Client = create_client(
-            self.url,
-            self.key
-        )
         try:
-            response = supabase.table('categories').insert({"name": category.name, "user_id": user_id}).execute()
+            response = self.supabase.table('categories').insert({"name": category.name, "user_id": user_id}).execute()
         except APIError as e:
             raise ManagerException("Failed to add category")
         if (response.status_code != 201):
             raise ManagerException("Failed to add category")
+    
+    def login(self, email, password):
+        response = self.supabase.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
+        data = response.model_dump()
+        user = User()
+        user.id = data['user']['id']
+        user.email = data['user']['email']
+        
+        try:
+            response = self.supabase.table('profiles').select("*").filter("user_id", "eq", data['user']['id']).execute()
+        except APIError:
+            raise ManagerException("Failed to fetch user data")
+        user.name = response.data[0]['name']
+
+        return user
+    
+    def get_user(self):
+        try:
+            response = self.supabase.auth.get_user()
+        except APIError:
+            raise ManagerException("Failed to fetch user")
+        data = response.model_dump()
+        user = User()
+        user.id = data['user']['id']
+        user.email = data['user']['email']
+
+        try:
+            response = self.supabase.table('profiles').select("*").filter("user_id", "eq", data['user']['id']).execute()
+        except APIError:
+            raise ManagerException("Failed to fetch user data")
+        user.name = response.data[0]['name']
+
+        return user
         
 
 
